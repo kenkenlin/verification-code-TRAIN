e1e70587fd1fbc38553515fdd063dc0379e66b75
x91003502 <32098103+x91003502@users.noreply.github.com>
2019-07-23 18:46:51 +0800

    NEW FILES

---
+++ captcha/config.py
@@ -0,0 +1,28 @@
+#-*- coding:utf-8 -*
+
+
+#原始路径
+path = 'C:\\Users\\Jack\\Documents\\GitHub\\captcha_ml'
+#训练集原始验证码文件存放路径
+captcha_path = path + '\\data\\captcha'
+#训练集验证码清理存放路径
+captcha_path_clean_path = path + '/data/captcha_clean'
+#训练集存放路径
+train_data_path = path + '/data/training_data'
+#模型存放路径
+model_path = path + '/data/model/model.model'
+#测试集原始验证码文件存放路径
+test_data_path = path + '/data/test_data'
+#测试结果存放路径
+output_path = path + '/data/result/result.txt'
+
+#识别的验证码个数
+image_character_num = 4
+
+#图像粗处理的灰度阈值
+threshold_grey = 100
+
+#标准化的图像大小
+image_width = 8
+image_height = 26
+
